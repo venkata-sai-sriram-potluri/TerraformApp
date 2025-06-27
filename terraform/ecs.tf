2aad63af34f6275960ccfe87df0101b1b9801c31
venkata-sai-sriram-potluri <venkatasaisrirampotluri@gmail.com>
2025-06-27 17:03:21 -0500

    main commit

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "11.0.0.0/16"
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "11.0.1.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 }
