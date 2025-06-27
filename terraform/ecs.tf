fd5aa91468e7e20b637d5c93ada431dd41fe58c9
venkata-sai-sriram-potluri <venkatasaisrirampotluri@gmail.com>
2025-06-27 14:46:39 -0500

    stage commit

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,10 +2,16 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
 }
 
