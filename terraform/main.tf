8fdaeaea52f9a8ee964172451e0ecefbaaf59fea
venkata-sai-sriram-potluri <venkatasaisrirampotluri@gmail.com>
2025-06-27 09:03:23 -0500

    staging workflow

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_db_instance" "mydb" {
+    identifier              = "my-python-db"
+    engine                  = "mysql"
+    instance_class          = "db.t3.micro"
+    engine_version          = "8.0.28"
+    allocated_storage       = 20
+    db_name                 = "myappdb"
+    username                = "User1"
+    password                = "Admin123"
+    publicly_accessible     = true
+    skip_final_snapshot     = true
+    vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  
+
+  tags = {
+    Name = "MyPythonAppDB"
+  }
+}
